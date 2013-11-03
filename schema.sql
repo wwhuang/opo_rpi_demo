093df88038b5d2fcb8fac900910817a7b28e64bc
William Huang <wwhuang@umich.edu>
2013-11-03 05:11:22 -0500

    fixes

--- schema.sql
+++ schema.sql
@@ -11,7 +11,7 @@ create table interactions (
 
 drop table if exists id_map;
 create table id_map (
-	pid integer primary key autoincrement;
+	pid integer primary key autoincrement,
 	ds2411_id integer not null,
 	node_id integer not null
 );
